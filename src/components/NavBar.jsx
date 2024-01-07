1a50e85522d173674dfdfde70a532d8e7bab47a8
el-sam <samurai.h@yahoo.com>
2024-01-07 20:27:46 +0800

    add the style and activeStyle to navlink

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,26 @@
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min"
 
 export default function NavBar(){
+
+    const style = {
+        color: "black",
+        fontFamily: "Lucida Console",
+        fontWeight: "bold"
+
+
+    }
+
+    const active = {
+        color:'red'
+
+    }
+
     return (
         <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/pokemonList">My Pokemon List</NavLink>
-            <NavLink to="/pokedex">Pokedex</NavLink>
-            <NavLink to="/aboutUs">About Us</NavLink>
+            <NavLink exact activeStyle={active} to="/" style={style}>Home</NavLink>
+            <NavLink activeStyle={active} to="/pokemonList" style={style}>My Pokemon List</NavLink>
+            <NavLink activeStyle={active} to="/pokedex" style={style}>Pokedex</NavLink>
+            <NavLink activeStyle={active} to="/aboutUs" style={style}>About Us</NavLink>
         </nav>
     )
 }
