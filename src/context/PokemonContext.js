33d8d8e4132906539da0a3ccc4077e43e36043e3
el-sam <samurai.h@yahoo.com>
2024-01-08 23:11:51 +0800

    create the PokemonContext passing the state in the context scrope allowing the children set the state sync the news changes/updates in the app

---
+++ src/context/PokemonContext.js
@@ -0,0 +1,15 @@
+import { useState,createContext } from "react";
+
+const PokemonContext = createContext()
+
+function PokemonContextProvider (children){
+    const [pokemons,setPokemons]=useState([])
+
+    return(
+        <PokemonContext.Provider value={[pokemons,setPokemons]}>
+            {children}
+        </PokemonContext.Provider>
+    )
+}
+
+export {PokemonContext,PokemonContextProvider}
