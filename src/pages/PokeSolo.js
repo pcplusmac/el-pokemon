3ad01cb8d9a6449a8a0f177951797ac61bdab42f
el-sam <samurai.h@yahoo.com>
2024-01-09 10:22:43 +0800

    add buttons to solo page

--- src/pages/PokeSolo.js
+++ src/pages/PokeSolo.js
@@ -2,15 +2,25 @@
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min"
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
 
-export default function PokeSolo(){
+export default function PokeSolo() {
 
-    const {state} = useLocation()
-    
+    const history = useHistory()
+
+    console.log("in solo:", history)
+    const { state } = useLocation()
+
     return (
-        <div>
-            <h1> poke sole page</h1>
-            <h2>{state.name}</h2>
+        <>
+            <div className="solopage-container">
+                <h1> poke sole page</h1>
+                <h2>{state.name}</h2>
 
-        </div>
+            </div>
+            <div className="button-container">
+                <button>back</button>
+                <button>save to myList</button>
+            </div >
+            
+        </>
     )
 }
