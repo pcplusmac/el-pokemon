f737658df26cbd789f229595caa5d99f096f4b15
el-sam <samurai.h@yahoo.com>
2024-01-09 13:09:29 +0800

    add margin/padding to solodetails container ; first attempt to show the pokemon deatails in <li>, by a function, still need to work on it later

--- src/pages/PokeSolo.js
+++ src/pages/PokeSolo.js
@@ -9,31 +9,45 @@ export default function PokeSolo() {
     console.log("in solo:", history)
     const { state } = useLocation()
 
-    const abilities = state.abilities.map(ability=>ability.name)
-    console.log("abilities",abilities)
+    const abilities = state.abilities.map(ability => { return ability.ability.name })
+    // console.log("abilities", abilities)
+
+    function listAbilities() {
+        for (const ability of abilities) {
+            return ability.ability.name
+
+        }
+    }
+
 
     return (
         <>
             <div>
                 <h2>{state.name}</h2>
-                <img id="card-solo" src={state.sprites.back_default} alt={state.name}/>
+                <img id="card-solo" src={state.sprites.back_default} alt={state.name} />
             </div>
-            
-            <div className="solodetails-container" style={{marginLeft:"2px"}}>
+
+            <div className="solodetails-container" style={{ marginLeft: "2px" }}>
+
+                <h3>Height:</h3>
+                <p>{state.height}</p>
+                <h3>Weight:</h3>
+                <p>{state.weight}</p>
+                <h3>Moves:</h3>
+                <p>{ }</p>
+                <h3>Ablities:</h3>
                 
-            
-                    <h3>Ablities:</h3>
-                    <ul>
-                        <li>{abilities}</li>
-                    </ul>
-            
+                <p>
+                    <li>{listAbilities}</li>
+                </p>
+
 
             </div>
             <div className="button-container">
                 <button>back</button>
                 <button>save to myList</button>
             </div >
-            
+
         </>
     )
 }
