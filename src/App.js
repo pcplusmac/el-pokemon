56083d37ea6f586dc3502cc3cea78882c3000ea8
el-sam <samurai.h@yahoo.com>
2024-01-09 09:43:17 +0800

    tidy the app.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,46 @@
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'; 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import Pokedex from './pages/Pokedex';
 import PokemonList from './pages/PokemonList';
 import Header from './components/Header';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import PokemondCard from './components/PokemonCard'
 import PokeSolo from './pages/PokeSolo';
+// import { PokemonContextProvider } from './context/PokemonContext';
 
 function App() {
 
-  const [pokeState,setPokeState] = useState([])
+  const [pokeState, setPokeState] = useState([])
 
-  async function fecthPokes(){
+  async function fecthPokes() {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon/")
     const data = await response.json()
-    console.log("first fetch:",data)
+    console.log("first fetch:", data)
     data.results.forEach(poke => fetchPokeDetails(poke.url))
   }
 
-  async function fetchPokeDetails(url){
+  async function fetchPokeDetails(url) {
     const res = await fetch(url)
     const data = await res.json()
-    setPokeState(currentState => [...currentState,data])
+    setPokeState(currentState => [...currentState, data])
   }
 
-  useEffect(()=> {fecthPokes()},[])
-  
-  const mappedPokeCards = pokeState.map((poke,index)=> <PokemondCard key={index} pokemon = {poke}/>)
-  
+  useEffect(() => {fecthPokes()},[])
 
+  const mappedPokeCards = pokeState.map((poke, index) => <PokemondCard key={index} pokemon={poke} />)
   return (
     <div className="App">
-      
       <Router>
-        <Header />
-        <Route exact path="/"><Home /></Route>
-        <Route path="/pokemonlist"><PokemonList  /></Route>
-        <Route exact path="/pokedex"><Pokedex pokeCards={mappedPokeCards}/></Route>
-        <Route path="/pokedex/:name" component={PokeSolo} ></Route>
-        <Route path="/aboutus"><AboutUs /></Route>
+        <Header />      
+          <Route exact path="/"><Home /></Route>
+          <Route path="/pokemonlist"><PokemonList /></Route>
+          <Route exact path="/pokedex">         
+              <Pokedex pokeCards={mappedPokeCards} /></Route>
+          <Route path="/pokedex/:name" component={PokeSolo} ></Route>
+          <Route path="/aboutus"><AboutUs /></Route>
       </Router>
-      
-      
-      
-      
-
-
-      
     </div>
   );
 }
