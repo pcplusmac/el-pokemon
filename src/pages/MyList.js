d11f728a83f5956c2a9663ced028c187d04f8352
pc-sam <samurai.h@yahoo.com>
2024-01-14 11:35:13 +0800

    try to route myList to individual pokemon by responding to the click event on li element

--- src/pages/MyList.js
+++ src/pages/MyList.js
@@ -1,9 +1,13 @@
 import { useState, useEffect, useContext } from 'react'
 import { PokemonContext } from "../context/PokemonContext"
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
+import PokemonDetails from '../components/PokemonDetails'
 
 
 
 export default function MyList() {
+    
+    const history = useHistory()
     const [pokemons, setPokemons] = useState([])
     console.log("mylist-pokemon:", pokemons)
 
@@ -17,6 +21,18 @@ export default function MyList() {
 
     useEffect(() => { fecthPokes() }, [])
 
+    function showCardClick(e){
+        console.log("myList:",e)
+
+        history.push(`/pokedex/${e.target.value}`)
+
+        // return (
+        //     <PokemonDetails />
+
+        // )
+
+    }
+
     return (
         <>
             <h1> My Pokemons:</h1>
@@ -24,7 +40,7 @@ export default function MyList() {
             <div className="list-container">
 
                 <ol>
-                    {pokemons.map((pokemon, index) => <li key={index}>{pokemon[index].name}</li>)}
+                    {pokemons.map((pokemon, index) => <li key={index} onClick={showCardClick}>{pokemon[index].name}</li>)}
                 </ol>
             </div>
         </>
